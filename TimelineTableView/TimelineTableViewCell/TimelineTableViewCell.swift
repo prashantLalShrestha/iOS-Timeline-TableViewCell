b9208524cc90c2b007fba538e9746eb52f8e292e
ram <ram@rams-MacBook-Air.local>
2017-11-22 18:31:20 +0545

    Initial Commit iOS Timeline TableView Cell

---
+++ TimelineTableView/TimelineTableViewCell/TimelineTableViewCell.swift
@@ -0,0 +1,121 @@
+//
+//  TimelineTableViewCell.swift
+//  TimelineTableView
+//
+//  Created by ram on 11/22/17.
+//  Copyright © 2017 ram. All rights reserved.
+//
+
+import UIKit
+
+class TimelineTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleInfoLabel: UILabel!
+    @IBOutlet weak var timeInfo: UILabel!
+    @IBOutlet weak var tagImage: UIImageView!
+    
+    // Point Variables
+    var pointPosition: CGPoint!
+    let pointDiameter: CGFloat = 6.0
+    let color = UIColor.black
+    
+    // Line Variables
+    let lineWidth: CGFloat = 2.0
+    let offset: CGFloat = 16
+    var (lineStart, lineMiddle, lineEnd) = (CGPoint(x: 0, y: 0), CGPoint(x: 0, y: 0), CGPoint(x: 0, y: 0))
+    
+    // Bubble Variables
+    let cornerRadius: CGFloat = 6.0
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+        timeRemainingLabel.sizeToFit()
+        titleInfoLabel.sizeToFit()
+        descriptionLabel.sizeToFit()
+        
+        pointPosition = CGPoint(x: (titleInfoLabel.frame.origin.x - offset + timeInfo.frame.maxX) / 2, y: titleInfoLabel.frame.origin.y + titleInfoLabel.intrinsicContentSize.height / 2 - pointDiameter / 2)
+        
+        lineStart = CGPoint(x: pointPosition.x + pointDiameter / 2, y: 0)
+        lineMiddle = CGPoint(x: lineStart.x, y: pointPosition.y)
+        lineEnd = CGPoint(x: lineStart.x, y: self.bounds.size.height)
+        
+        drawLine(from: lineStart, to: lineMiddle, color: color)
+        drawLine(from: lineMiddle, to: lineEnd, color: color)
+        
+        
+        drawPoint()
+        
+        if let title = titleInfoLabel.text, !title.isEmpty {
+            drawBubble()
+        }
+    }
+    
+    fileprivate func drawPoint() {
+        let path = UIBezierPath(ovalIn: CGRect(x: pointPosition.x, y: pointPosition.y, width: pointDiameter, height: pointDiameter))
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = color.cgColor
+        shapeLayer.fillColor = color.cgColor
+        shapeLayer.lineWidth = lineWidth
+        
+        self.contentView.layer.addSublayer(shapeLayer)
+    }
+    
+    fileprivate func drawBubble() {
+        
+        let bubbleRect = CGRect(
+            x: titleInfoLabel.frame.origin.x - offset / 2,
+            y: titleInfoLabel.frame.origin.y - offset / 2,
+            width: titleInfoLabel.intrinsicContentSize.width + offset,
+            height: titleInfoLabel.intrinsicContentSize.height + offset)
+        
+        let path = UIBezierPath(roundedRect: bubbleRect, cornerRadius: cornerRadius)
+        let startPoint = CGPoint(x: bubbleRect.origin.x, y: bubbleRect.origin.y + bubbleRect.height / 2 - 8)
+        path.move(to: startPoint)
+        path.addLine(to: startPoint)
+        path.addLine(to: CGPoint(x: bubbleRect.origin.x - 8, y: bubbleRect.origin.y + bubbleRect.height / 2))
+        path.addLine(to: CGPoint(x: bubbleRect.origin.x, y: bubbleRect.origin.y + bubbleRect.height / 2 + 8))
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.fillColor = UIColor.lightGray.cgColor
+        
+        self.contentView.layer.insertSublayer(shapeLayer, below: titleInfoLabel.layer)
+    }
+    
+    fileprivate func drawLine(from: CGPoint, to: CGPoint, color: UIColor) {
+        let path = UIBezierPath()
+        path.move(to: from)
+        path.addLine(to: to)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = color.cgColor
+        shapeLayer.lineWidth = lineWidth
+        
+        self.contentView.layer.addSublayer(shapeLayer)
+    }
+    
+    func configureCellData(timeInfo: String, title: String, description: String, timeRemaining: String) {
+        self.timeInfo.text = timeInfo
+        self.titleInfoLabel.text = title
+        self.descriptionLabel.text = description
+        self.timeRemainingLabel.text = timeRemaining
+    }
+    
+}
---
+++ TimelineTableView/ViewController.swift
@@ -0,0 +1,128 @@
+//
+//  ViewController.swift
+//  TimelineTableView
+//
+//  Created by ram on 11/22/17.
+//  Copyright © 2017 ram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var datas = [
+        "21st Dec 2017": [
+            [
+                "timeInfo": "11:30",
+                "title": "Work",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at iaculis velit, eu ullamcorper ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam egestas porta convallis. Praesent vehicula justo vitae ex placerat, ut lobortis elit fringilla.",
+                "timeRemaining": "10 mins"
+            ],
+            [
+                "timeInfo": "15:30",
+                "title": "Meeting",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at iaculis velit, eu ullamcorper ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam egestas porta convallis.",
+                "timeRemaining": "120 mins"
+            ],
+            [
+                "timeInfo": "13:30",
+                "title": "Lunch With Friends",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "timeRemaining": "125 mins"
+            ],
+            [
+                "timeInfo": "21:30",
+                "title": "Meeting",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at iaculis velit, eu ullamcorper ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+                "timeRemaining": "15 mins"
+            ]
+        ],
+        "22nd Dec 2017": [
+            [
+                "timeInfo": "13:30",
+                "title": "Lunch With Friends",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "timeRemaining": "125 mins"
+            ],
+            [
+                "timeInfo": "21:30",
+                "title": "Meeting",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at iaculis velit, eu ullamcorper ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+                "timeRemaining": "15 mins"
+            ],
+            [
+                "timeInfo": "14:30",
+                "title": "Lunch With Friends",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "timeRemaining": "125 mins"
+            ],
+            [
+                "timeInfo": "22:30",
+                "title": "Greeting",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at iaculis velit, eu ullamcorper ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+                "timeRemaining": "15 mins"
+            ]
+        ]
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let timelineTableViewCellNib = UINib(nibName: "TimelineTableViewCell", bundle: nil)
+        tableView.register(timelineTableViewCellNib, forCellReuseIdentifier: "timelineTableViewCell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return datas.keys.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let value = datas[Array(datas.keys.sorted())[section]] {
+            return value.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Array(datas.keys.sorted())[section]
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.setNeedsLayout()
+        view.backgroundColor = #colorLiteral(red: 0.9685459733, green: 0.9686813951, blue: 0.9685031772, alpha: 1)
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "timelineTableViewCell", for: indexPath) as! TimelineTableViewCell
+        if let values = datas[Array(datas.keys.sorted())[indexPath.section]]{
+            let value = values[indexPath.row]
+            if let timeInfo = value["timeInfo"], let title = value["title"], let description = value["description"], let timeRemaining = value["timeRemaining"] {
+                cell.timeInfo.text = timeInfo
+                cell.titleInfoLabel.text = title
+                cell.descriptionLabel.text = description
+                cell.timeRemainingLabel.text = timeRemaining
+            }
+        }
+        return cell
+    }
+
+}
+
